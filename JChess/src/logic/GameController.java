3924fac8f06d9e73c6e1fd5469ff07ed5f2a38e1
benjaminfrey363 <109392270+benjaminfrey363@users.noreply.github.com>
2023-04-16 11:57:07 -0600

    Update GameController.java

--- JChess/src/logic/GameController.java
+++ JChess/src/logic/GameController.java
@@ -1,5 +1,21 @@
 package logic;
 
+// Controls flow of the game.
 public class GameController {
+	private static Logic logic;
+	
+	// Play a game of chess.
+	public static void playGame() {
+		// Creat new logic, thus creating and displaying UI.
+		logic = new Logic();
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		playGame();
+	}
+	
 
 }
+
