7d2110a8d2a681e3c540960caf28b5f2f0113b04
benjaminfrey363 <109392270+benjaminfrey363@users.noreply.github.com>
2023-04-16 11:57:46 -0600

    Update Piece.java

--- JChess/src/logic/Piece.java
+++ JChess/src/logic/Piece.java
@@ -1,5 +1,16 @@
 package logic;
 
 public class Piece {
-
+	public String tag;
+	// Horizontal and vertical coordinates.
+	public int x;
+	public int y;
+	public Piece(int x, int y) {
+		// For all piece coordinates, x represents row and y
+		// represents column.
+		this.x = x; this.y= y;
+	}
+	public Piece(int x, int y, String tag) {
+		this.x = x; this.y = y; this.tag = tag;
+	}
 }
